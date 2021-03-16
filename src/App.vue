5879fa66c84447d6c19f29a55116eefee8d92ba0
Aleksey Belchenkov <u608110@gmail.com>
2021-03-16 22:48:20 +0400

    DELETE, Remind Task throw Backend

--- src/App.vue
+++ src/App.vue
@@ -54,13 +54,30 @@ export default {
           data
       ];
     },
-    deleteTask(id) {
+    async deleteTask(id) {
       if (confirm('Are you sure?')) {
-        this.tasks = this.tasks.filter(task => task.id !== id);
+        const res = await fetch(`api/tasks/${id}`, {
+          method: 'DELETE'
+        });
+
+        res.status === 200 ? (this.tasks = this.tasks.filter(task => task.id !== id)) : alert('Error Deleting Task');
       }
     },
-    toggleReminder(id) {
-      this.tasks = this.tasks.map(task => task.id === id ? {...task, reminder: !task.reminder} : task);
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id);
+      const updTask = {...taskToToggle, reminder: !taskToToggle.reminder};
+
+      const res = await fetch('api/tasks', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updTask)
+      });
+
+      const data = await res.json();
+
+      this.tasks = this.tasks.map(task => task.id === id ? {...task, reminder: data.reminder} : task);
     },
     toggleAddTask() {
       this.showAddTask = !this.showAddTask;
